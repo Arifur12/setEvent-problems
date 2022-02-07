5cdfdb59e15900115197464d0e31c8accf225b2d
arif <abcvpc@gmail.com>
2022-02-07 21:43:39 +0600

    useState show hide

--- src/App.js
+++ src/App.js
@@ -1,34 +1,47 @@
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  
-  const [events, setEvents]= useState([
-    {title: 'Mario and Luigi', id:1},
-    {title: 'Mario and DK', id:2},
-    {title: 'Wario', id:3}
+  const [showEvents, setShowEvents] = useState(true);
+  const [events, setEvents] = useState([
+    { title: "Mario and Luigi", id: 1 },
+    { title: "Mario and DK", id: 2 },
+    { title: "Wario", id: 3 },
   ]);
-  const handleClick= (id) =>{
-    
-    setEvents((prevEvent)=>{
-      return prevEvent.filter((event)=>{
-        console.log(prevEvent);
-        return id  !== event.id;
-        
-      })
-    })
+  console.log(showEvents);
+  const handleClick = (id) => {
+    setEvents((prevEvent) => {
+      return prevEvent.filter((event) => {
+        return id !== event.id;
+      });
+    });
     console.log(id);
-    
-  }
+  };
 
   return (
     <div className="App">
-      {events.map((event,index)=>(
-        <div key={event.id}>
-          <h1>{event.title} and index is {event.id}</h1>
-          <button onClick={()=>handleClick(event.id)}>delete me</button>
+      {!showEvents && (
+        // use bracket after {useState && (jsx)}
+        <div>
+          <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
-      ))}  
+      )}
+      {showEvents && (
+        <div>
+          <button onClick={() => setShowEvents(false)}>Hide Events</button>
+        </div>
+      )}
+
+      {showEvents &&
+        events.map((event, index) => (
+          // if useState true then it shows
+          <div key={event.id}>
+            <h1>
+              {event.title} and index is {event.id}
+            </h1>
+            <button onClick={() => handleClick(event.id)}>delete me</button>
+          </div>
+        ))}
     </div>
   );
 }
